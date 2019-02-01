822e4ea3df5f240ebf2238de9cf1da34d334289b
Musuauergit config --global user.email musuauer@gmail.com <info@guillermogudino.com>
2019-02-01 09:09:12 +0100

    separate subtitle

--- src/App.js
+++ src/App.js
@@ -32,7 +32,19 @@ class App extends Component {
 
       const header = content.data.mediaContainers[0].metadata.map(data => data.content)
 
-      const subtitle = header[1].split(/\b(?![\?\.\!])/)
+      const subtitleArray = header[1].split('.')
+
+      const subtitle = subtitleArray.map((item, i, arr) => {
+        let divider = i < subtitleArray.length - 1 && <br />
+        return (
+          <span >
+            <div key={i}>{item}</div>
+            {divider}
+          </span>
+        )
+      })
+
+      console.log('subtitle', subtitle)
 
       const footer = content.data.metadata[1].content
 
@@ -44,7 +56,7 @@ class App extends Component {
 
       this.setState({
         title: header[0],
-        subtitle: header[1].match(/[^\.!\?]+[\.!\?]+/g),
+        subtitle: subtitle,
         footer,
         images
       })
